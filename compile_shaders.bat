5d44cfa9632e0106dd59163c666e19826d0070dc
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2019-02-20 16:45:46 +0100

    Add a simple batch script to compile all shaders

---
+++ compile_shaders.bat
@@ -0,0 +1,6 @@
+%VULKAN_SDK%\Bin\glslangValidator.exe -V data\shaders\static_entity.vert -o data\shaders\static_entity.vert.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V data\shaders\static_entity.frag -o data\shaders\static_entity.frag.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V data\shaders\dynamic_entity.vert -o data\shaders\dynamic_entity.vert.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V data\shaders\dynamic_entity.frag -o data\shaders\dynamic_entity.frag.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V data\shaders\debug_draw.vert -o data\shaders\debug_draw.vert.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V data\shaders\debug_draw.frag -o data\shaders\debug_draw.frag.spv
